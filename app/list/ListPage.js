682e1c7e4ac7551fbf020763b697eb7376429e13
顾飞 <feigu@pptv.com>
2018-10-16 19:56:43 +0800

    增加请求接口显示列表数据

--- app/list/ListPage.js
+++ app/list/ListPage.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 
-import {Text, StyleSheet, View, FlatList} from 'react-native'
+import {Text, StyleSheet, View, FlatList, ActivityIndicator} from 'react-native'
 
 
 export default class ListPage extends Component {
@@ -10,24 +10,61 @@ export default class ListPage extends Component {
 
     constructor(props) {
         super(props);
+        this.state = {isLoading: true}
+    }
+
+
+    componentDidMount() {
+        return fetch('http://api.apiopen.top/musicBroadcasting')
+            .then((response) => {
+                if (response.status === 200) {
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong on api server!');
+                }
+            })
+            .then(json => {
+                console.debug(json);
+                console.log('data=' + json.result.toString());
+                // let json = responseJson.toJSON();
+                const array =json.result;
+                var arr = [];
+                for (let i = 0, size = array.size; i < size; i++) {
+                    let objArray = array.get('channellist');
+                    for (let j = 0, size = objArray.size; j < size; i++) {
+                        let obj = objArray.get(j);
+                        arr.push(obj);
+                        console.log('data=' + obj.toString());
+                    }
+                }
+
+                this.setState({
+                    isLoading: false,
+                    dataSource: array,
+                }, function () {
+
+                });
+
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }
 
     render() {
+        if (this.state.isLoading) {
+            return (<View style={{flex: 1, padding: 20}}>
+                <ActivityIndicator/>
+            </View>);
+        }
+
         return (
             <View style={styles.container}>
                 <FlatList
-                    data=
-                        {[
-                            {key: 'Devin'},
-                            {key: 'Jackson'},
-                            {key: 'James'},
-                            {key: 'Joel'},
-                            {key: 'John'},
-                            {key: 'Jillian'},
-                            {key: 'Jimmy'},
-                            {key: 'Julie'},
-                        ]}
-                    renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+                    data={
+                        this.state.dataSource
+                    }
+                    renderItem={({item}) => <Text style={styles.item}>{item.title}</Text>}
                 />
             </View>
         );
